f65028bf8862ff9bd7e1ba2cb4d099eeb197eedb
mbaruthn <mbaruthn@hotmail.com>
2023-10-12 21:02:21 +0300

    Setup

---
+++ README.md
@@ -0,0 +1,65 @@
+# Model Training
+
+To train the model to be used with appropriate labels on the CustumVision service,
+you need to host it within the model folder and write the labels into the classes array.
+The output model from CustumVision should be in the format of OnnxF16, GeneralCompact[S1].
+
+# ONNX-ObjectDetectionAIApi
+
+It performs inference on a single photo within the payload,
+detecting the objects defined.
+
+# Generating API Application Output
+
+To obtain the output of a Windows application, you can use pyinstaller. After making all the necessary changes and conducting your API tests, you can prepare your application by running the following command:
+```
+pyinstaller --onefile --add-data "C:\Users\sadfgasdfs\openvino_env\Lib\site-packages\openvino\libs;." --noconsole --add-data "[{C:\Users\sadfgasdfs\AppData\Local\Programs\Python\}]Python39\Lib\site-packages\openvino;." --add-binary "C:\Users\sadfgasdfs\AppData\Local\Programs\Python\Python39\Lib\site-packages\openvino\inference_engine\constants.pyd;."E:\docker\src\vision\app.py
+
+```
+This command will create a ready-to-use .exe application.
+
+## Performance Benchmark
+
+- CPU: Intel Core i7-8700 
+- 2 images in each request
+
+| Method | Float Precision | Platform | Average Latency | Throughput |
+|---|---|---|---|---|
+| Batch Processing | FP16 | ONNX | 105 ms | 19 FPS |
+| Sequential Processing (Default) | FP32 | ONNX | 71 ms | 28 FPS |
+| Batch Processing | FP32 | ONNX | 67 ms | 30 FPS |
+
+<--------------------------------------------------------------------------------------------------------------->
+
+# Model Eğitimi
+
+Kullanılacak olan modeli CustumVision servisi üzerinde uygun etiketler ile eğitimini yapıp, 
+model klasörü içerisinde barındırmanız ve etiketleri classes arrayı içerisine yazmanız yeterli olacaktır.
+CustumVision üzerinden OnnxF16, GeneralCompact[S1] model çıktısı almanız gerekmektedir.
+
+# ONNX-ObjectDetectionAIApi
+
+Payload içerisindeki tek bir fotoğraf üzerinde Inference yapar.
+Tanımlı olan nesneleri bulur.
+
+# API Uygulama Çıktısı Hazırlama
+
+Bir windows uygulaması çıktısı almak için pyinstaller kullanabilirsiniz.
+Tüm değişiklikleri ve API denemelerinizi gerçekleştirdikten sonra,
+```
+pyinstaller --onefile --add-data "C:\Users\sadfgasdfs\openvino_env\Lib\site-packages\openvino\libs;." --noconsole --add-data "[{C:\Users\sadfgasdfs\AppData\Local\Programs\Python\}]Python39\Lib\site-packages\openvino;." --add-binary "C:\Users\sadfgasdfs\AppData\Local\Programs\Python\Python39\Lib\site-packages\openvino\inference_engine\constants.pyd;."E:\docker\src\vision\app.py
+
+```
+komutu ile .exe uygulamanızı hazır hale getirebilirsiniz.
+
+
+## Performace Benchmark
+
+- CPU: Intel Core i7-8700 
+- 2 images in each request
+
+| Method | Float Precision | Platform | Average Latency | Throughput |
+|---|---|---|---|---|
+| Batch Processing | FP16 | ONNX | 105 ms | 19 FPS |
+| Sequential Processing (Default) | FP32 | ONNX | 71 ms | 28 FPS |
+| Batch Processing | FP32 | ONNX | 67 ms | 30 FPS |
---
+++ src/vision/app.py
@@ -0,0 +1,111 @@
+import numpy as np
+import onnx
+import onnxruntime
+import PIL.Image
+import time
+from aioflask import Flask, jsonify, request
+
+# Flask uygulamasını oluşturma
+app = Flask(__name__)
+
+# Sabitler ve sınıf etiketleri
+PROB_THRESHOLD = 0.1
+classes = ["bottle", "bottle_hand", "other"]
+
+# Model sınıfı oluşturma
+class Model:
+    def __init__(self, model_filepath):
+        self.session = onnxruntime.InferenceSession(str(model_filepath))
+        assert len(self.session.get_inputs()) == 1
+        self.input_shape = self.session.get_inputs()[0].shape[2:]
+        self.input_name = self.session.get_inputs()[0].name
+        self.input_type = {'tensor(float)': np.float32, 'tensor(float16)': np.float16}[self.session.get_inputs()[0].type]
+        self.output_names = [o.name for o in self.session.get_outputs()]
+
+        self.is_bgr = False
+        self.is_range255 = False
+        onnx_model = onnx.load(model_filepath)
+        for metadata in onnx_model.metadata_props:
+            if metadata.key == 'Image.BitmapPixelFormat' and metadata.value == 'Bgr8':
+                self.is_bgr = True
+            elif metadata.key == 'Image.NominalPixelRange' and metadata.value == 'NominalRange_0_255':
+                self.is_range255 = True
+
+    def predict(self, image):
+        image = image.resize(self.input_shape)
+        input_array = np.array(image, dtype=np.float32)[np.newaxis, :, :, :]
+        input_array = input_array.transpose((0, 3, 1, 2))  # => (N, C, H, W)
+        if self.is_bgr:
+            input_array = input_array[:, (2, 1, 0), :, :]
+        if not self.is_range255:
+            input_array = input_array / 255  
+
+        outputs = self.session.run(self.output_names, {self.input_name: input_array.astype(self.input_type)})
+        return {name: outputs[i] for i, name in enumerate(self.output_names)}
+
+# Flask uygulaması başlatılırken modeli yükleyin
+model = None
+def runOD(model_filepath):
+    global model
+    model = Model(model_filepath)
+    if model is None:
+        print("Model NOT FOUND!")
+
+# REST API endpoints
+@app.route("/", methods=["GET"])
+async def index():
+    return jsonify(
+        {
+            "ServiceName": "AcoRecycle Object Detection Vision API",
+            "version": "0.2.0",
+            "Environment": "Development",
+        }
+    )
+
+@app.route("/", methods=["POST"])
+async def main():
+    global model
+    image_files = request.files.getlist("image_files")
+    response = {
+        "results": None,
+        "message": None,
+    }
+
+    if model is None:
+        response["message"] = "Model not found"
+    else:
+        global elapsed
+        filenames = []
+        for image_file in image_files:
+            filenames.append(image_file.filename)
+            imageF = PIL.Image.open(image_file)
+        
+        start_time = time.time()  
+        predictions = model.predict(imageF)
+        end_time = time.time()
+        
+        elapsed = end_time - start_time
+        results_list = []
+        for i in range(len(filenames)):
+            prediction_dict = {"filename": filenames[i], "predictions": []}
+            assert set(predictions.keys()) == set(['detected_boxes', 'detected_classes', 'detected_scores'])
+            
+            for class_id, score in zip(predictions['detected_classes'][0], predictions['detected_scores'][0]):
+                if score > PROB_THRESHOLD:
+                    classes_final = classes[class_id]
+                    score_final = score
+                    prediction_dict["predictions"].append({"tagName": classes_final, "probability": score_final})
+            
+            results_list.append(prediction_dict)
+        response["results"] = results_list
+        response["message"] = "OK"
+        
+    print(elapsed)
+    return jsonify(response)
+
+# Ana uygulamayı başlatma
+if __name__ == "__main__":
+    # Modelin bulunacağı sabit yolu belirtiyoruz
+    model_filepath = "./model/model.onnx"
+    runOD(model_filepath)
+    app.run(debug=False, host="0.0.0.0", port=8100)
